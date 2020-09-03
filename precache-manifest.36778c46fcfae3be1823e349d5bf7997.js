a65eaa373dba15aa99f497320acb168d1c1af21f
AreYall7 <ryall.edwards7@gmail.com>
2020-09-03 15:14:46 -0500

    Updates

---
+++ precache-manifest.36778c46fcfae3be1823e349d5bf7997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf25effc7d2d576d6d6d258639ae200a",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "2a239f3b14c7c3f9f47b",
+    "url": "/Recipe-App/static/css/main.a203259b.chunk.css"
+  },
+  {
+    "revision": "6d67894d5ab4d3145ad6",
+    "url": "/Recipe-App/static/js/2.20dedf64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-App/static/js/2.20dedf64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a239f3b14c7c3f9f47b",
+    "url": "/Recipe-App/static/js/main.a0248b8a.chunk.js"
+  },
+  {
+    "revision": "c035ec1896e8fb26f1a6",
+    "url": "/Recipe-App/static/js/runtime-main.5105d1b0.js"
+  }
+]);
